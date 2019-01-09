04c9edfca02a4fb64c18a9178ac71b6cf92c0a2d
Margaret Curtis <margaret.curtis@yodle.com>
2019-01-08 23:21:25 -0500

    two tests passing, missing cases

--- string.js
+++ string.js
@@ -1,2 +1,10 @@
 String.prototype.endsWith = function(search, position) {
+	const difference = String.prototype.length - search.length - 1;
+	const originalValue = String.prototype.substring(difference);
+	if (search.match(originalValue)) {
+		return true;
+	} else {
+		return false;
+	}
 }
+
