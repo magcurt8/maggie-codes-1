3673d4e0ccd1a35d3033530006ce83719262a630
Margaret Curtis <margaret.curtis@yodle.com>
2019-01-09 00:44:30 -0500

    array-work find and includes completed

--- array.js
+++ array.js
@@ -1,8 +1,22 @@
 Array.prototype.find = function(fn) {
+	var result
+	this.forEach(function(element){
+		if (fn(element)) {
+			result = element
+		}
+	})
+	return result
 }
 
 Array.prototype.findIndex = function(fn) {
 }
 
 Array.prototype.includes = function(search) {
+	var result = false
+	this.forEach(function(element) {
+		if (element == search) {
+			result = true
+		}
+	})
+	return result
 }
